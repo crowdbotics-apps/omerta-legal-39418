c5f2bec7a307da6742229568f4065d2dbf746791
Crowdbotics <team@crowdbotics.com>
2023-03-16 17:23:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/omerta_legal_39418/MainActivity.java
@@ -0,0 +1,15 @@
+package com.omerta_legal_39418;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "omerta_legal_39418";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "omerta-legal-39418",
+  url: "https://omerta-legal-39418.botics.co",
+  api: "https://omerta-legal-39418.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
